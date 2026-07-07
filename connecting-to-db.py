d2fb73e1da6382e977ed81dd424c5c00effc3b8e
ahmadyusuf <yusufahmadnaim@gmail.com>
2026-07-07 14:22:28 +0300

    Practiced this technical lesson

--- connecting-to-db.py
+++ connecting-to-db.py
@@ -1,2 +1,21 @@
 import sqlite3
 import pandas as pd
+
+conn = sqlite3.connect('data.sqlite')
+
+cur = conn.cursor()
+
+cur.execute("""select * from data""")
+table_names = cur.fetchall()
+print(table_names)
+
+conn.close()
+
+conn = sqlite3.connect('data.sqlite')
+
+df= pd.read_sql_query("SELECT * FROM sqlite_master WHERE type='table'", conn) 
+type(df)
+pd.core.frame.DataFrame
+pd.read_sql("SELECT * FROM offices", conn)
+conn.close()
+
